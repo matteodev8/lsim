ab6a110db6e08e1a53731dcc91fd7fe5b6af81fc
matteodev8 <me@matteodev.xyz>
2025-03-17 20:18:55 +0100

    'AND' and 'OR' gates implemented

---
+++ README.md
@@ -0,0 +1,5 @@
+# lsim
+
+logic sim in rust and bevy
+
+If it doesn't want to run due to wgpu errors, try setting `WGPU_ALLOW_UNDERLYING_NONCOMPLIANT_ADAPTER=1`
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use bevy::{color::palettes::basic::WHITE, prelude::*};
+use sim::gates::Gate;
 use util::input::{InputPlugin, Mouse};
 
+pub mod sim;
 pub mod util;
 
 fn main() {
-    App::new()
-        .add_plugins((DefaultPlugins, InputPlugin))
-        .add_systems(Startup, setup)
-        .add_systems(Update, draw_cursor)
-        .run();
+    // App::new()
+    //     .add_plugins((DefaultPlugins, InputPlugin))
+    //     .add_systems(Startup, setup)
+    //     .add_systems(Update, draw_cursor)
+    //     .run();
+
+    let switch_a = Box::new(sim::gates::Input { state: true });
+    let switch_b = Box::new(sim::gates::Input { state: false });
+    let lamp = sim::gates::OrGate::new(switch_a, switch_b).simulate();
+
+    println!("{}", lamp)
 }
 
 fn draw_cursor(mut gizmos: Gizmos, mouse: Res<Mouse>) {
